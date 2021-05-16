c890c79d0fe02bc3d3ab508267328fc5cdf8d807
tibiw <tibor.waxmann@gmail.com>
2021-05-16 23:11:29 +0300

    Initial Commit

---
+++ EyesColor/EyesColorApp.swift
@@ -0,0 +1,17 @@
+//
+//  EyesColorApp.swift
+//  EyesColor
+//
+//  Created by Tibor Waxmann on 16.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct EyesColorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
